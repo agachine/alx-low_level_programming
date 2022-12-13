c12bea59e355dd766be9b2e496d6e18286ad700b
agachine <gachine.alvin@gmail.com>
2022-12-13 07:03:08 -0800

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main (void) /*printing a string using outs function */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 char My_string[] = "\"Programming is like building a multilingual puzzle";
 puts(My_string);
-return(0);
+return (0);
 }
