b034ca42930dae8da869383b277960e562951a6e
agachine <58729213+agachine@users.noreply.github.com>
2022-12-17 05:41:03 +0300

    Create 8-print_square.c
    prints square made of #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_square - prints square made of #
+* @size: length of side of square
+* Return: nothing
+*/
+void print_square(int size)
+{
+int n, m;
+if (size > 0)
+{
+for (n = 0; n < size; n++)
+{
+for (m = 0; m < size; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
