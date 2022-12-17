13210a18249b327e214a7a8068fa589237416797
agachine <58729213+agachine@users.noreply.github.com>
2022-12-17 19:10:41 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
 void print_most_numbers(void)
 {
 int n;
-for (n = '0'; n <= '9'; n++)
+for (n = 0; n <= 9; n++)
 {
-if ((n != '4') || (n != '2))
+if ((n != 4) || (n != 2))
 {
-_putchar(n);
+_putchar((n % 10) + '0');
 }
 }
 _putchar('\n');
