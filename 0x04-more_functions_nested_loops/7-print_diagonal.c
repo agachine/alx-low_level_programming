d45f115b30483eae409f2bcb1b461251bc2ba3df
agachine <58729213+agachine@users.noreply.github.com>
2022-12-17 05:36:56 +0300

    Create 7-print_diagonal.c
    prints a diagonal of size n

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal of size n
+ * @n: length of diagonal
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+if (n > 0)
+{
+int a, b;
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < a; b++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+else
+_putchar('\n')
+}
