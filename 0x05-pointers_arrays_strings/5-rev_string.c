d20ef886c40f0a25afc57fece8094f1b8afa9536
agachine <58729213+agachine@users.noreply.github.com>
2022-12-20 16:52:38 +0300

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* rev_string - reverses a string
+*@s: string to be printed in reverse
+* Return: nothing.
+*/
+void rev_string(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+{
+c++;
+}
+while (c > 0)
+{
+c--;
+_putchar(s[c]);
+}
+_putchar('\n');
+}
