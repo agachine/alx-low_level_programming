e9856dfd30cca80b215d94a02b87cba5acb7fc57
agachine <58729213+agachine@users.noreply.github.com>
2022-12-20 15:52:23 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
 * reset_to_98 - Takes a pointer and updates the value it points to to 98
-*@*n: Pointer to be updated
+*@n: Pointer to be updated
 * Return: Always 0.
 */
 void reset_to_98(int *n)
