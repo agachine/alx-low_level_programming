77f2345594e6d98acfae272d9f279551442b0f6d
agachine <58729213+agachine@users.noreply.github.com>
2022-12-20 16:19:09 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - prints a string
+*@str: string to be printed 
+* Return: nothing.
+*/
+void _puts(char *str)
+{
+_putchar(str);
+_putchar('\n');
+}
