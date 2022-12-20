3925cbe7d004be7744281d3c91123dc75cc23d4b
agachine <58729213+agachine@users.noreply.github.com>
2022-12-20 17:35:53 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,20 @@
 */
 void puts_half(char *str)
 {
-  int n;
+int n;
 int i = 0;
 while (str[i] != '\0')
 {
 i++;
 }
-if (i % 2 == 0) 
+if (i % 2 == 0)
 n = i / 2;
-else 
-n = ((i - 1) /2);
-  
-while ( n <= i)
+else
+n = ((i + 1) /2);
+while (n <= i)
 {
 _putchar(str[n]);
 n++;
 }
+_putchar('\n'); 
 }
