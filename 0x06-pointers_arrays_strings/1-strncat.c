4ce6f7c00f89f09a5bc6333ec78ca7a589180a90
agachine <gachine.alvin@gmail.com>
2022-12-21 00:59:01 -0800

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,17 +9,18 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
+int j;
 int i = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
-int j = 0;
+j = 0;
 while (src[j] != '\0' && j < n)
 {
 dest[i] = src[j];
 i++;
 j++;
 }
-returns (dest);
+return (dest);
 }
