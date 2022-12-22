c5460d289648d31e778a50d01017d15d9bfaf4bc
agachine <58729213+agachine@users.noreply.github.com>
2022-12-22 10:09:53 +0300

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strcat - concatenates two strings
+* @a: an array of integers
+* @n: the number of elements to swap
+* Return: Always 0.
+*/
+void reverse_array(int *a, int n)
+{
+int temp;
+int i = 0;
+while (i == n)
+{
+temp[] = a[i]
+i--;
+}
+a[i] = temp []
+}
