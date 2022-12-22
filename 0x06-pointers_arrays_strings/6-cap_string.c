3f71396e7f7abf5335a8b3edaf8393ac7db6248c
agachine <58729213+agachine@users.noreply.github.com>
2022-12-22 11:37:28 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,11 +9,14 @@ char *cap_string(char *str)
 {
 char a[] = {9, 10, 32, 44, 59, 46, 33, 69, 34, 40, 41, 125, 123};
 int j; 
+for (i = 0; str[i] != '\0'; i++)
+{
 for (j = 0; a[j] != '\0'; j++)  
 {
 if ( a[j] == str[i] && str[i + 1] >= 97 && str[i + 1] <= 122) 
 {
 str[i + 1] = str[i + 1] - 32;
+}
 }
 }
 return (str);
