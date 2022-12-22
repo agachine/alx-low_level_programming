7db1c5304f0ad0741ddc436fbdce3a48920abe22
agachine <58729213+agachine@users.noreply.github.com>
2022-12-22 11:55:10 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,10 @@ int j;
 int i;
 for (i = 0; str[i] != '\0'; i++)
 {
-
+if (i == 0 && str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+}
 for (j = 0;  a[j] != '\0'; j++)  
 {
 if (a[j] == str[i] && str[i + 1] >= 97 && str[i + 1] <= 122) 
