891ca01026923042357c5d8d3b77997c63436aff
agachine <58729213+agachine@users.noreply.github.com>
2022-12-22 10:45:28 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,13 @@
 char *string_toupper(char *)
 {
 int i;
-char str; 
-for (i = 0; str[i] != '\o'; i++)
+char str[]; 
+for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] >= 'a' && str[i] <= 'a')
 {
 str[i] = str[i] - 32; 
 }
 }
-return (str);
+return str;
 }
