f8ec3caefb2e16abdbaac588b44e4f721d339acb
agachine <58729213+agachine@users.noreply.github.com>
2022-12-22 09:08:56 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,17 +4,19 @@
 * _strcat - concatenates two strings
 * @dest: input parameter string
 * @src: input parameter string
+* @n: 
 * Return: Always 0.
 */
 char *_strncat(char *dest, char *src, int n)
 {
 int j = 0;
 int i = 0;
-while (dest[i] != '\0')
+while (dest[i] != '\0' && j < n)
 {
 dest[i] = src[j];
 i++;
 j++;
 }
+dest[i] = '\0';
 return (dest);
 }
