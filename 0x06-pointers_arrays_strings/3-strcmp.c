dc92d93135d106b532d3fa20933e8dda7b84f07d
agachine <58729213+agachine@users.noreply.github.com>
2022-12-22 09:32:50 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,9 @@
 * _strcmp - compares two strings
 * @s1: input parameter string
 * @s2: input parameter string 
-* Return: Always 0.
+* Return: value
 */
-int _strcmp(char *s1, char *s2
+int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 while (s1[i] != '\0' && s2[j] != '\0')
