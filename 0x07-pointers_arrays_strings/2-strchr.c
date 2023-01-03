5afaf3f4c885b0a273a4f6832ca3622506d49bd5
agachine <58729213+agachine@users.noreply.github.com>
2023-01-03 20:22:52 +0300

    Create 2-strchr.c
    locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @c: character to be located
+ * @s: pointer where new substring is placed
+ * Return: s if c is found else NULL
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; i < *(s + i); i++)
+{
+if (*(s + i) == c)
+return (s + i);
+}
+return (0);
+}
