b2c1a5f92ea0bdb439dbff17eaa1939ef524cda6
agachine <gachine.alvin@gmail.com>
2023-01-09 09:58:10 -0800

    program prints its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* main: print program name
-* @argc: argument count
-* @argv[]: string
-* Return: nothing 
+* Description - Program prints its own name
+*main - print program name
+*@argc: argument count
+*@argv: argument string
+*Return: nothing
 */
 int main(int __attribute__((unused)) argc, char *argv[])
 {
