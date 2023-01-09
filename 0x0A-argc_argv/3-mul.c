1b8c7b491985f263de5d06eba2f118e7aa7d48bc
agachine <gachine.alvin@gmail.com>
2023-01-09 10:33:27 -0800

    program prints its own name

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - print program arguments
+* @argc: argument count
+* @argv: string
+* Return: nothing
+*/
+int main(int  argc, char  *argv[])
+{
+int mul;
+if (argc > 2)
+{
+mul= atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", mul);
+}
+else
+printf("Error\n");
+return (1);
+}
