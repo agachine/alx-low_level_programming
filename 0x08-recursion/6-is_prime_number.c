ee8dd2bc236085d08d803b2641c8000c546cce97
agachine <gachine.alvin@gmail.com>
2023-01-09 04:40:31 -0800

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,46 +1,31 @@
 #include "main.h"
-
 int checkprime (int n , int i);
-
+/**
+ * checkprime - finds prime number
+ * is_prime_number finds prime number
+ * @n: number to be checked
+ * @i: divider
+ * Return: return 1 if input is prime, else 0
+ */ 
 int checkprime (int n , int i)
-  
 {
-  
-  if ( n < 2)
-    
-    {
-      
-      return 0;
-      
-    }
-  
-  if (n == 2 || n == 3)
-    
-    return (1);
-  
-  if (n % i == 0)
-    
-    {
-      
-      return (0);
-      
-    }
-  
-  if ( i > n/2)
-    
-    return (1);
-  
-  else
-    return  (checkprime  (n , i+1));
-  
+if (n < 2)
+{
+return 0;
 }
-
-
-
-int is_prime_number(int n)
-  
+if (n == 2 || n == 3)
+return (1);
+if (n % i == 0)
 {
- int i = 2;
- return (checkprime (n , i));
-    
-    }
+return (0);
+}
+if ( i > n/2)
+return (1);
+else
+ return  (checkprime  (n , i+1));
+}
+int is_prime_number(int n)
+{
+int i = 2;
+return (checkprime (n , i));
+}
---
+++ 0x08-recursion/pow.c
@@ -0,0 +1,29 @@
+#include "main.h"
+int _pow_recursion(int x, int y)
+{
+
+  if (y < 0)
+    
+    {
+    return (-1);
+       } 
+  if (y == 0)
+    {
+
+      return (1);
+
+    }
+  else if (y == 1)
+    {
+      return (x);
+    }
+  return x * _pow_recursion(x, y-1); 
+
+ 
+
+
+
+
+}
+
+
