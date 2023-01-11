a3bf6957d10bfc28917bf40d2bd76d06f9b5f766
agachine <58729213+agachine@users.noreply.github.com>
2023-01-11 22:16:21 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,11 +22,11 @@ while (s1[i] != '\0')
 i++;
 }
 j = 0;
-while (s2[i] != '\0')
+while (s2[j] != '\0')
 {
-i++;
+j++;
 }
-i = i  + 1;
+i = i + j  + 1;
 p = (char *)malloc(sizeof(char) * i);
 if (p == NULL)
 {
