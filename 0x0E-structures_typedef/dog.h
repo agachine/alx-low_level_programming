1673a457b2f62866dfef6ad0b47d8f3ed8c44a19
agachine <gachine.alvin@gmail.com>
2023-01-18 04:30:06 -0800

    Initialize a variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <stddef.h>
+#ifndef DOG_H
+#define DOG_H
 /**
 *struct dog - dog details structure
 *@name: dog's name
@@ -6,9 +8,14 @@
 *@owner: dog's owner
 *Return: nothing
 */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
