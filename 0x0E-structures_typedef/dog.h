b622b99369f7e2321911ecc6fa1916b29878a75d
agachine <gachine.alvin@gmail.com>
2023-01-18 03:53:25 -0800

    structures syntax

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*struct dog - dog details structure
+*@name: dog's name
+*@age: dog's age
+*@owner: dog's owner
+*Return: nothing
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
