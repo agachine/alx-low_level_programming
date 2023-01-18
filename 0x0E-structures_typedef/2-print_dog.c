3d8cfe89b9149aa40efa67def56cd141a01a2474
agachine <58729213+agachine@users.noreply.github.com>
2023-01-18 17:22:38 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,20 +12,20 @@ if (d != NULL)
 {
 if (d->name == NULL)
 {
-printf("Name: (nil)");
+printf("Name: (nil)\n");
 }
 else 
 { 
 printf("Name:%s", d->name);   
 }
-printf("Age:%f", d->age);      
+printf("Age:%f\n", d->age);      
 if ( d->owner == NULL)
 {
-printf("Owner: (nil)");
+printf("Owner: (nil)\n");
 }
 else 
 { 
-printf("Owner:%s", d->owner);
+printf("Owner:%s\n", d->owner);
 }
 }
 }
