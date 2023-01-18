8801cd3901e87d1582a351d47a1b47363ab8730a
agachine <58729213+agachine@users.noreply.github.com>
2023-01-18 17:54:59 +0300

    frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+*free_dog - free memory allocated to dog function
+*@d: dog memory allocation
+*Return: nothing
+*/
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->owner);
+free(d->name);
+free(d);
+} 
+}
