dff3b1872961bfac492a06c26f72951e1a8886d1
agachine <58729213+agachine@users.noreply.github.com>
2023-01-19 13:43:00 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,12 @@ include <stdio.h>
 /**
  * print_name - prints a name
  * @name: name of the person
- *
+ *@f: pointer to theprinting function
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (!name || !f)
+return;
 f (name); 
 }
