cbc7fd1d876caf31d56e76018652e838cb85bd12
agachine <58729213+agachine@users.noreply.github.com>
2023-01-23 00:45:49 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * struct op - Struct op
  *
@@ -13,9 +12,8 @@
 typedef struct op
 {
 char *op;
-int (*f)(int a, int b);
+ int (*f)(int a, int b);
 } op_t;
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
