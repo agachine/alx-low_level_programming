03706cec5c087ba5e68458e2258f8a1c755b2be2
agachine <58729213+agachine@users.noreply.github.com>
2023-02-03 10:39:53 +0300

    adds a new node at the beginning of a linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,12 +7,12 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *temp;
-temp = malloc(sizeof(listint_t));
-if (temp == NULL)
+listint_t *new;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
 return (NULL);
-temp->n = n;
-temp->next = *head;
-*head = temp;
-return (temp);
+new->n = n;
+new->next = head;
+head = new;
+return (new);
 }
