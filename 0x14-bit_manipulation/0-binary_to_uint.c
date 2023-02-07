e2ad71dd6f5c8d63980de9451e3df2d4814e38d7
agachine <gachine.alvin@gmail.com>
2023-02-07 08:56:45 -0800

    printf project

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * int binary_to_uint - converts a binary number to an unsigned int
+ *
+ * Return: converted number or 0 if it fails 
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int dec_num = 0;
+int i;
+if (b == NULL)
+return (0);
+i = 0;
+while (b[i] != '\0')
+{
+if (b[i] == '0' || b[i] == '1')
+{
+dec_num <<= 1;
+dec_num += b[i] - '0';
+i++;
+}
+else 
+return (0);
+}
+return(dec_num);
+}
